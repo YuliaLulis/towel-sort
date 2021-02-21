bdcbfd74af4fb3b79fe47ffa5b17c517cbff516e
YuliaLulis <lulis_job@mail.ru>
2021-02-22 00:16:51 +0300

    fix: add implementation of task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  }
+  var res = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    var mat = [];
+    if ((i + 2) % 2 !== 0) {
+      mat = matrix[i].reverse();
+    } else {
+      mat = matrix[i];
+    }
+
+    for (let j = 0; j < mat.length; j++) {
+      res.push(mat[j]);
+    }
+
+
+
+  }
+  return res;
+
+
+
 }
